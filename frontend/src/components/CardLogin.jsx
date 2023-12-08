ae8459129443612b8994eabcc2794e8f656c7229
Simón Posada Acosta <simon.150@hotmail.com>
2023-12-08 18:00:30 -0500

    mensajes de error

--- frontend/src/components/CardLogin.jsx
+++ frontend/src/components/CardLogin.jsx
@@ -1,6 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { loginUserThunk, setCreateUser } from "../features/users/userSlice";
+import {
+  loginUserThunk,
+  setCreateUser,
+  setMessageError,
+} from "../features/users/userSlice";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 
@@ -8,20 +12,27 @@ export const CardLogin = () => {
   const { register, handleSubmit } = useForm();
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
-  const { createUser, isAuthenticated, loginSucces } = useSelector(
+  const { createUser, isAuthenticated, messageError } = useSelector(
     (state) => state.UserLogin
   );
+  const [activeMessage, setActiveMessage] = useState(false);
 
   useEffect(() => {
     if (isAuthenticated) {
       navigate("/search");
     }
-  }, [isAuthenticated]);
+
+    if (messageError !== null) {
+      setActiveMessage(true);
+      setTimeout(() => {
+        setActiveMessage(false);
+        dispatch(setMessageError(null));
+      }, 1500);
+    }
+  }, [isAuthenticated, messageError]);
 
   const onSubmit = handleSubmit((values) => {
     dispatch(loginUserThunk(values));
-    console.log(values);
   });
 
   const handleChangeCreate = () => {
@@ -80,6 +91,27 @@ export const CardLogin = () => {
           </button>
         </div>
       </div>
+      {activeMessage && (
+        <div
+          role="alert"
+          className="alert alert-error fixed top-[92%] w-1/2 right-1"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="stroke-current shrink-0 h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <span>{messageError}</span>
+        </div>
+      )}
     </div>
   );
 };
